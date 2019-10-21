fbe2c038af318f85fdad2c458097d4bb2008e449
Cezary Piątek <cezarypiatek@live.com>
2019-10-21 20:59:38 +0200

    Add logging with Serilog

--- src/HappyCode.NetCoreBoilerplate.Api/Program.cs
+++ src/HappyCode.NetCoreBoilerplate.Api/Program.cs
@@ -1,6 +1,10 @@
+using System;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
 
 namespace HappyCode.NetCoreBoilerplate.Api
 {
@@ -8,12 +12,40 @@ namespace HappyCode.NetCoreBoilerplate.Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            Log.Logger = CreateLogger();
+
+            try
+            {
+                Log.Logger.Information("Starting up");
+                using var webHost = CreateWebHostBuilder(args).Build();
+                webHost.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Fatal(ex, "Application start-up failed");
+                Log.CloseAndFlush();
+                throw;
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureServices(services => services.AddAutofac())
                 .UseStartup<Startup>();
+
+        private static Logger CreateLogger()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddJsonFile("appsettings.local.json", optional: true)
+                .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            return logger;
+        }
     }
 }
