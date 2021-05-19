6e83b15469c4495c92385076744b963ed67daa38
Lukasz Kurzyniec <lkurzyniec@gmail.com>
2021-05-19 13:00:03 +0200

    small refactor around UpgradeOptions in HappyCode.NetCoreBoilerplate.Db

--- src/HappyCode.NetCoreBoilerplate.Db/Options/UpgradeOptions.cs
+++ src/HappyCode.NetCoreBoilerplate.Db/Options/UpgradeOptions.cs
@@ -3,6 +3,6 @@ namespace HappyCode.NetCoreBoilerplate.Db.Options
     public class UpgradeOptions
     {
         public int CommandExecutionTimeoutSeconds { get; set; }
-        public string RunPattern { get; set; }
+        public string ScriptsAndCodePattern { get; set; }
     }
 }
--- src/HappyCode.NetCoreBoilerplate.Db/Program.cs
+++ src/HappyCode.NetCoreBoilerplate.Db/Program.cs
@@ -10,17 +10,15 @@ namespace HappyCode.NetCoreBoilerplate.Db
 {
     class Program
     {
-        private static readonly Regex _defaultRunPattern = new Regex(@".*", RegexOptions.Compiled);
-
         public static int Main(string[] args)
         {
             var configuration = LoadAppConfiguration();
             var upgradeOptions = configuration.GetSection("UpgradeOptions").Get<UpgradeOptions>();
-            var runPattern = TryGetRunPattern(upgradeOptions.RunPattern) ?? _defaultRunPattern;
+            var scriptsAndCodePatternPattern = new Regex(upgradeOptions.ScriptsAndCodePattern ?? ".*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
 
             var upgrader = DeployChanges.To
                 .SqlDatabase(configuration.GetConnectionString("MsSqlDb"))
-                .WithScriptsAndCodeEmbeddedInAssembly(Assembly.GetExecutingAssembly(), (fileName) => runPattern.IsMatch(fileName))
+                .WithScriptsAndCodeEmbeddedInAssembly(Assembly.GetExecutingAssembly(), (fileName) => scriptsAndCodePatternPattern.IsMatch(fileName))
                 .WithExecutionTimeout(TimeSpan.FromSeconds(upgradeOptions.CommandExecutionTimeoutSeconds))
                 .WithTransaction()
                 .LogToConsole()
@@ -39,9 +37,6 @@ namespace HappyCode.NetCoreBoilerplate.Db
             Console.WriteLine("Success!");
             return 0;
         }
-
-        private static Regex TryGetRunPattern(string candidateRegeix)
-            => string.IsNullOrWhiteSpace(candidateRegeix) ? null : new Regex(candidateRegeix);
 
         private static IConfigurationRoot LoadAppConfiguration()
         {
