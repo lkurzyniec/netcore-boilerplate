75a6d4ce6483a765d501aadb0e74476163d2a439
Łukasz Kurzyniec <5943484+lkurzyniec@users.noreply.github.com>
2023-01-22 23:04:32 +0100

    Dotnet7 upgrade (#95)
    * moved to Central Package Management
    
    * migration to dotnet 7
    
    * packages upgrade
    
    * code fix after upgrade
    
    * fixed docker-compose and prepopulation of mssql

--- db/mssql/docker-db-init.sh
+++ db/mssql/docker-db-init.sh
@@ -1,6 +1,7 @@
-#wait for the SQL Server to come up
-sleep 30s
+#!/bin/bash
 
-echo "running set up script"
-#run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P SomeStrongPwd123 -d master -i db-init.sql
+echo "waiting 20s for the SQL Server to come up..."
+sleep 20s
+
+echo "running set up script..."
+/opt/mssql-tools/bin/sqlcmd -S "localhost" -U sa -P "${MSSQL_SA_PASSWORD}" -d master -i db-init.sql
