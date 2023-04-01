ddc99075b43af410b14706a82b66138dd0fc9fde
Łukasz Kurzyniec <lkurzyniec@gmail.com>
2023-04-01 20:56:33 +0200

    added PeriodicTimer to BackgroundService

--- src/HappyCode.NetCoreBoilerplate.Api/BackgroundServices/PingWebsiteBackgroundService.cs
+++ src/HappyCode.NetCoreBoilerplate.Api/BackgroundServices/PingWebsiteBackgroundService.cs
@@ -8,6 +8,7 @@ namespace HappyCode.NetCoreBoilerplate.Api.BackgroundServices
 {
     public class PingWebsiteBackgroundService : BackgroundService
     {
+        private readonly PeriodicTimer _timer;
         private readonly HttpClient _client;
         private readonly ILogger<PingWebsiteBackgroundService> _logger;
         private readonly IOptions<PingWebsiteSettings> _configuration;
@@ -20,24 +21,28 @@ namespace HappyCode.NetCoreBoilerplate.Api.BackgroundServices
             _client = httpClientFactory.CreateClient(nameof(PingWebsiteBackgroundService));
             _logger = logger;
             _configuration = configuration;
+             _timer = new PeriodicTimer(TimeSpan.FromMinutes(_configuration.Value.TimeIntervalInMinutes));
         }
 
         protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                _logger.LogInformation($"{nameof(PingWebsiteBackgroundService)} running at '{DateTime.Now}', pinging '{_configuration.Value.Url}'");
+                _logger.LogInformation("{BackgroundService} running at '{Date}', pinging '{URL}'",
+                    nameof(PingWebsiteBackgroundService), DateTime.Now, _configuration.Value.Url);
                 try
                 {
                     using var response = await _client.GetAsync(_configuration.Value.Url, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
-                    _logger.LogInformation($"Is '{_configuration.Value.Url.Authority}' responding: {response.IsSuccessStatusCode}");
+                    _logger.LogInformation("Is '{Host}' responding: {Status}",
+                        _configuration.Value.Url.Authority, response.IsSuccessStatusCode);
                 }
                 catch (Exception ex)
                 {
                     _logger.LogWarning(ex, "Error during ping");
                 }
-                await Task.Delay(TimeSpan.FromMinutes(_configuration.Value.TimeIntervalInMinutes), cancellationToken);
+                await _timer.WaitForNextTickAsync(cancellationToken);
             }
+            _timer.Dispose();
         }
     }
 }
