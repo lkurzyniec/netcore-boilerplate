c5ea04e4147977d709e7a36b61e4d9e6f9de2524
Łukasz Kurzyniec <5943484+lkurzyniec@users.noreply.github.com>
2023-04-09 13:13:17 +0200

    Architectural tests (#139)
    * added Core arch tests
    
    * added more arch tests
    
    * rename to better express intention
    
    * image after rename

---
+++ test/HappyCode.NetCoreBoilerplate.ArchitecturalTests/ApiArchitecturalTests.cs
@@ -0,0 +1,43 @@
+using FluentAssertions;
+using HappyCode.NetCoreBoilerplate.Api;
+using HappyCode.NetCoreBoilerplate.Api.Controllers;
+using NetArchTest.Rules;
+using Xunit;
+
+namespace HappyCode.NetCoreBoilerplate.ArchitecturalTests
+{
+    public class ApiArchitecturalTests
+    {
+        private static readonly Types _apiTypes = Types.InAssembly(typeof(Startup).Assembly);
+
+        [Fact]
+        public void Controllers_should_inherit_from_ApiControllerBase()
+        {
+            var result = _apiTypes
+                .That()
+                .ResideInNamespace("HappyCode.NetCoreBoilerplate.Api.Controllers")
+                .And()
+                .AreNotAbstract()
+                .Should()
+                .Inherit(typeof(ApiControllerBase))
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Controllers_should_have_Controller_suffix()
+        {
+            var result = _apiTypes
+                .That()
+                .ResideInNamespace("HappyCode.NetCoreBoilerplate.Api.Controllers")
+                .And()
+                .AreNotAbstract()
+                .Should()
+                .HaveNameEndingWith("Controller")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+    }
+}
---
+++ test/HappyCode.NetCoreBoilerplate.ArchitecturalTests/CoreArchitecturalTests.cs
@@ -0,0 +1,93 @@
+using FluentAssertions;
+using HappyCode.NetCoreBoilerplate.Core;
+using Microsoft.EntityFrameworkCore;
+using NetArchTest.Rules;
+using Xunit;
+
+namespace HappyCode.NetCoreBoilerplate.ArchitecturalTests
+{
+    public class CoreArchitecturalTests
+    {
+        private static readonly Types _coreTypes = Types.InAssembly(typeof(FeatureFlags).Assembly);
+
+        [Fact]
+        public void Core_should_not_use_AspNetCore()
+        {
+            var result = _coreTypes
+                .ShouldNot()
+                .HaveDependencyOn("Microsoft.AspNetCore")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Repositories_should_not_use_Services()
+        {
+            var result = _coreTypes
+                .That()
+                .ResideInNamespace("HappyCode.NetCoreBoilerplate.Core.Repositories")
+                .ShouldNot()
+                .HaveDependencyOn("HappyCode.NetCoreBoilerplate.Core.Services")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Repositories_should_have_Repository_suffix()
+        {
+            var result = _coreTypes
+                .That()
+                .ResideInNamespace("HappyCode.NetCoreBoilerplate.Core.Repositories")
+                .And()
+                .AreNotAbstract()
+                .Should()
+                .HaveNameEndingWith("Repository")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Services_should_have_Service_suffix()
+        {
+            var result = _coreTypes
+                .That()
+                .ResideInNamespace("HappyCode.NetCoreBoilerplate.Core.Services")
+                .And()
+                .AreNotAbstract()
+                .Should()
+                .HaveNameEndingWith("Service")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Dto_should_have_Dto_suffix()
+        {
+            var result = _coreTypes
+                .That()
+                .ResideInNamespace("HappyCode.NetCoreBoilerplate.Core.Dtos")
+                .Should()
+                .HaveNameEndingWith("Dto")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void DbContext_should_have_Context_suffix()
+        {
+            var result = _coreTypes
+                .That()
+                .Inherit(typeof(DbContext))
+                .Should()
+                .HaveNameEndingWith("Context")
+                .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+    }
+}
