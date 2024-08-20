92347b72c723b8da3a8acb439512e1965d8aa89d
Łukasz Kurzyniec <lkurzyniec@gmail.com>
2024-08-20 15:49:21 +0200

    added version to banner

--- src/HappyCode.NetCoreBoilerplate.Api/Infrastructure/Configurations/BannerConfigurator.cs
+++ src/HappyCode.NetCoreBoilerplate.Api/Infrastructure/Configurations/BannerConfigurator.cs
@@ -6,21 +6,22 @@ public static class BannerConfigurator
     private const string _reverse = "\x1b[7m";
     private const string _clear = "\x1b[39m\x1b[27m";
 
-    private const string _banner = @"{0}
-            _                         _           _ _                 _       _
+    private const string _banner = @"{1}
+            _                         _           _ _                 _       _ v{0}
  _ __   ___| |_ ___ ___  _ __ ___    | |__   ___ (_) | ___ _ __ _ __ | | __ _| |_ ___
 | '_ \ / _ \ __/ __/ _ \| '__/ _ \___| '_ \ / _ \| | |/ _ \ '__| '_ \| |/ _` | __/ _ \
 | | | |  __/ || (_| (_) | | |  __/___| |_) | (_) | | |  __/ |  | |_) | | (_| | ||  __/
 |_| |_|\___|\__\___\___/|_|  \___|   |_.__/ \___/|_|_|\___|_|  | .__/|_|\__,_|\__\___|
-                                                               |_| {1}by Łukasz Kurzyniec
-{2}";
+                                                               |_| {2}by Łukasz Kurzyniec
+{3}";
 
     public static void Print(bool withColors)
     {
+        string version = Environment.GetEnvironmentVariable("HC_VERSION") ?? "local";
         string banner = withColors switch
         {
-            true => string.Format(_banner, _cyan, _reverse, _clear),
-            false => string.Format(_banner, null, null, null),
+            true => string.Format(_banner, version, _cyan, _reverse, _clear),
+            false => string.Format(_banner, version, null, null, null),
         };
         Console.WriteLine(banner);
     }
