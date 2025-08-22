8dedf74571743b0ccba05f70b22c992409f89e87
Łukasz Kurzyniec <lkurzyniec@gmail.com>
2025-08-22 09:30:14 +0200

    fixed MockQueryable

--- test/HappyCode.NetCoreBoilerplate.Core.UnitTests/Extensions/EnumerableExtensions.cs
+++ test/HappyCode.NetCoreBoilerplate.Core.UnitTests/Extensions/EnumerableExtensions.cs
@@ -2,22 +2,15 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using MockQueryable.Moq;
-using Moq;
 
 namespace HappyCode.NetCoreBoilerplate.Core.UnitTests.Extensions
 {
     internal static class EnumerableExtensions
     {
-        public static Mock<DbSet<TEntity>> GetMockDbSet<TEntity>(this IEnumerable<TEntity> source)
-            where TEntity : class
-        {
-            return source.AsQueryable().BuildMockDbSet();
-        }
-
         public static DbSet<TEntity> GetMockDbSetObject<TEntity>(this IEnumerable<TEntity> source)
             where TEntity : class
         {
-            return source.GetMockDbSet().Object;
+            return source.ToList().BuildMockDbSet().Object;
         }
     }
 }
