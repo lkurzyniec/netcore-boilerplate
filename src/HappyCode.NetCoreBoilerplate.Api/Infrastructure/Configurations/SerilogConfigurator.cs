b798bae9a96ac353421e30a5df9239e313fb9109
Łukasz Kurzyniec <lkurzyniec@gmail.com>
2025-11-04 09:18:12 +0100

    adjust around request logging

--- src/HappyCode.NetCoreBoilerplate.Api/Infrastructure/Configurations/SerilogConfigurator.cs
+++ src/HappyCode.NetCoreBoilerplate.Api/Infrastructure/Configurations/SerilogConfigurator.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using HappyCode.NetCoreBoilerplate.Api.Infrastructure.Logging;
 using Microsoft.Extensions.Configuration;
 using Serilog;
@@ -11,13 +12,15 @@ namespace HappyCode.NetCoreBoilerplate.Api.Infrastructure.Configurations
     [ExcludeFromCodeCoverage]
     public static class SerilogConfigurator
     {
+        private static readonly IEnumerable<string> _pathsToOmit = ["/healthz", "/api-doc", "/openapi", "/favicon"];
+
         public static Logger CreateLogger()
         {
             var configuration = LoadAppConfiguration();
             return new LoggerConfiguration()
                 .ReadFrom.Configuration(configuration)
                 .Filter.ByExcluding(Matching.WithProperty("RequestPath", "/"))
-                .Filter.ByIncludingOnly(e => e.Properties.TryGetValue("RequestPath", out var path) ? path.ToString().Contains("/healthz") && e.Level >= LogEventLevel.Warning : true)
+                .Filter.ByIncludingOnly(e => e.Properties.TryGetValue("RequestPath", out var path) ? _pathsToOmit.Any(o => path.ToString().Contains(o)) ? e.Level >= LogEventLevel.Warning : true : true)
                 .MinimumLevel.Override("Microsoft.AspNetCore.Hosting.Diagnostics", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.AspNetCore.Routing.EndpointMiddleware", LogEventLevel.Warning)
                 .Enrich.With(new VersionEnricher(new()))
