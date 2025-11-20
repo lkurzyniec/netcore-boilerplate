28cc020750949f18fb4a533b4002c758f1585dfc
Łukasz Kurzyniec <lkurzyniec@gmail.com>
2025-11-20 18:50:54 +0100

    removed MinimalApis.Extensions dependency

--- src/HappyCode.NetCoreBoilerplate.BooksModule/Dtos/BookDto.cs
+++ src/HappyCode.NetCoreBoilerplate.BooksModule/Dtos/BookDto.cs
@@ -2,4 +2,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HappyCode.NetCoreBoilerplate.BooksModule.Dtos;
 
-public record struct BookDto (int? Id, [property: Required] string Title);
+public record BookDto(
+    int? Id,
+
+    [property: Required]
+    string Title
+);
